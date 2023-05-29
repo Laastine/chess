639a3019d32e8a59c3df6171a789ec6ab4eddc60
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2023-05-29 20:28:41 +0300

    Add unit tests for simple pawn and knight movements

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ const BLACK_ROOK: isize = -5;
 const BLACK_QUEEN: isize = -9;
 const BLACK_KING: isize = -std::isize::MAX;
 
-
+#[derive(PartialEq, Debug)]
 struct Movement {
     from: (usize, usize),
     to: (usize, usize),
@@ -23,8 +23,35 @@ struct Movement {
     en_passant: bool,
 }
 
+fn square_to_index(square: &str) -> (usize, usize) {
+    let mut chrs = square.chars();
+    let sq: (char, &str) = match chrs.next() {
+        Some(c) => (c, chrs.as_str()),
+        None => panic!("Can not convert square to")
+    };
+    let row =
+        match sq.0 {
+            'a' => 1,
+            'b' => 2,
+            'c' => 3,
+            'd' => 4,
+            'e' => 5,
+            'f' => 6,
+            'g' => 7,
+            'h' => 8,
+            _ => panic!("Illegal row notation"),
+        };
+
+    let column: usize = match sq.1.parse::<usize>() {
+        Ok(c) => c,
+        Err(e) => panic!("Illegal column notation {}", e),
+    };
+
+    (row, column)
+}
+
 impl Movement {
-    pub fn parse_movement_notation(&self, movement: &str) -> Movement {
+    pub fn parse_movement_notation(movement: &str) -> Movement {
         let parts: Vec<&str> = movement.split_ascii_whitespace().collect();
         let (from_notation, to_notation): (&str, &str) = if parts.len() != 2 {
             panic!("Illegal move notation")
@@ -32,8 +59,8 @@ impl Movement {
             (parts[0], parts[1])
         };
 
-        let from = self.square_to_index(from_notation);
-        let to = self.square_to_index(to_notation);
+        let from = square_to_index(from_notation);
+        let to = square_to_index(to_notation);
 
         Movement {
             from,
@@ -43,37 +70,31 @@ impl Movement {
             en_passant: false,
         }
     }
+}
 
-    fn square_to_index(&self, square: &str) -> (usize, usize) {
-        let mut chrs = square.chars();
-        let foo: (char, &str) = match chrs.next() {
-            Some(c) => (c, chrs.as_str()),
-            None => panic!("Can not convert square to")
-        };
-        let row = self.row_to_index(foo.0);
+#[cfg(test)]
+mod test {
+    use crate::Movement;
 
-        let column: usize = match foo.1.parse::<usize>() {
-            Ok(c) => c,
-            Err(e) => panic!("Illegal column notation {}", e),
-        };
-
-        (row, column)
+    #[test]
+    fn test_pawn_movement_notation() {
+        let mov = Movement::parse_movement_notation("e2 e4");
+        assert_eq!(mov.from, (5, 2));
+        assert_eq!(mov.to, (5, 4));
+        assert_eq!(mov.en_passant, false);
+        assert_eq!(mov.castle, false);
     }
 
-    fn row_to_index(&self, chr: char) -> usize {
-        match chr {
-            'a' => 0,
-            'b' => 1,
-            'c' => 2,
-            'd' => 3,
-            'e' => 4,
-            'f' => 5,
-            'g' => 6,
-            'h' => 7,
-            _ => panic!("Illegal row notation"),
-        }
+    #[test]
+    fn test_knight_movement_notation() {
+        let mov = Movement::parse_movement_notation("b1 c3");
+        assert_eq!(mov.from, (2, 1));
+        assert_eq!(mov.to, (3, 3));
+        assert_eq!(mov.en_passant, false);
+        assert_eq!(mov.castle, false);
     }
 }
+
 
 // 0,0 equals square A1 and white rook,
 struct Board {
@@ -94,11 +115,6 @@ impl Board {
                 [BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN],
             ]
         }
-    }
-
-    fn movement_to_board_state(&mut self, movement: &str) -> [[isize; 8]; 8] {
-
-        self.board
     }
 }
 
