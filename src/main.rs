271a22c6f199a907d0f7cc419a3bf2d431325e4f
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2023-05-31 20:26:13 +0300

    Add out-of-bounds check to movement

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,23 @@ const BLACK_ROOK: isize = -5;
 const BLACK_QUEEN: isize = -9;
 const BLACK_KING: isize = -99;
 
-#[derive(PartialEq, Debug)]
-struct Movement {
-    from: (usize, usize),
-    to: (usize, usize),
-    capture: bool,
-    castle: bool,
-    en_passant: bool,
+pub type Move = (usize, usize);
+
+#[derive(Debug, PartialEq)]
+pub enum MoveError {
+    IllegalMovementNotation,
+    FromSquareOutOfBoard,
+    ToSquareOutOfBoard,
 }
 
-fn square_to_index(square: &str) -> (usize, usize) {
+#[derive(PartialEq, Debug)]
+struct Movement {
+    from: Move,
+    to: Move,
+    capture: bool,
+}
+
+fn square_to_index(square: &str) -> Move {
     let mut chrs = square.chars();
     let sq: (char, &str) = match chrs.next() {
         Some(c) => (c, chrs.as_str()),
@@ -51,10 +58,10 @@ fn square_to_index(square: &str) -> (usize, usize) {
 }
 
 impl Movement {
-    pub fn parse_movement_notation(movement: &str) -> Movement {
+    pub fn parse_movement_notation(movement: &str) -> Result<Movement, MoveError> {
         let parts: Vec<&str> = movement.split_ascii_whitespace().collect();
         let (from_notation, to_notation): (&str, &str) = if parts.len() != 2 {
-            panic!("Illegal move notation")
+            return Err(MoveError::IllegalMovementNotation)
         } else {
             (parts[0], parts[1])
         };
@@ -62,42 +69,49 @@ impl Movement {
         let from = square_to_index(from_notation);
         let to = square_to_index(to_notation);
 
-        Movement {
+        if !Movement::is_legal_move(to) {
+            return Err(MoveError::ToSquareOutOfBoard)
+        }
+
+        if !Movement::is_legal_move(from) {
+            return Err(MoveError::FromSquareOutOfBoard)
+        }
+
+        Ok(Movement {
             from,
             to,
             capture: false,
-            castle: false,
-            en_passant: false,
-        }
+        })
+    }
+
+    fn is_legal_move(square: Move) -> bool {
+        square.0 <= 8 &&
+            square.1 <= 8
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{BLACK_QUEEN, BLACK_ROOK, Board, EMPTY_SQUARE, Movement, WHITE_KNIGHT, WHITE_PAWN, WHITE_QUEEN, WHITE_ROOK};
+    use crate::{Board, EMPTY_SQUARE, MoveError, Movement, WHITE_KNIGHT, WHITE_PAWN};
 
     #[test]
     fn pawn_movement_notation() {
-        let mov = Movement::parse_movement_notation("e2 e4");
+        let mov = Movement::parse_movement_notation("e2 e4").unwrap();
         assert_eq!(mov.from, (4, 2));
         assert_eq!(mov.to, (4, 4));
-        assert_eq!(mov.en_passant, false);
-        assert_eq!(mov.castle, false);
     }
 
     #[test]
     fn knight_movement_notation() {
-        let mov = Movement::parse_movement_notation("b1 c3");
+        let mov = Movement::parse_movement_notation("b1 c3").unwrap();
         assert_eq!(mov.from, (1, 1));
         assert_eq!(mov.to, (2, 3));
-        assert_eq!(mov.en_passant, false);
-        assert_eq!(mov.castle, false);
     }
 
     #[test]
     fn pawn_movement_to_board_square_index() {
         let mut board = Board::new();
-        let mov = Movement::parse_movement_notation("e2 e4");
+        let mov = Movement::parse_movement_notation("e2 e4").unwrap();
 
         assert_eq!(board.get_piece_by_movement(mov.from), WHITE_PAWN);
         assert_eq!(board.get_piece_by_movement(mov.to), EMPTY_SQUARE);
@@ -109,15 +123,25 @@ mod test {
     #[test]
     fn knight_movement_to_board_square_index() {
         let mut board = Board::new();
-        let mov = Movement::parse_movement_notation("b1 c3");
+        let mov = Movement::parse_movement_notation("b1 c3").unwrap();
 
-        dbg!(mov.from.0,mov.from.1);
-        dbg!(mov.to.0,mov.to.1);
         assert_eq!(board.get_piece_by_movement(mov.from), WHITE_KNIGHT);
         assert_eq!(board.get_piece_by_movement(mov.to), EMPTY_SQUARE);
         board.update_board(&mov);
         assert_eq!(board.get_piece_by_movement(mov.from), EMPTY_SQUARE);
         assert_eq!(board.get_piece_by_movement(mov.to), WHITE_KNIGHT);
+    }
+
+    #[test]
+    fn check_legal_movement_notation() {
+        let mov = Movement::parse_movement_notation("b1c1");
+        assert_eq!(mov, Err(MoveError::IllegalMovementNotation))
+    }
+
+    #[test]
+    fn check_out_of_bounds_movement() {
+        let mov = Movement::parse_movement_notation("b1 c11");
+        assert_eq!(mov, Err(MoveError::ToSquareOutOfBoard))
     }
 }
 
@@ -143,7 +167,7 @@ impl Board {
         }
     }
 
-    fn movement_coord_to_board_coord(square: (usize, usize)) -> (usize, usize) {
+    fn movement_coord_to_board_coord(square: Move) -> Move {
         let col = Board::get_rev_index(square.0);
         let row = Board::get_rev_index(square.1 - 1);
         (row, col)
@@ -153,7 +177,6 @@ impl Board {
         let (row, col) = Board::movement_coord_to_board_coord(square);
         self.board[row][col]
     }
-
 
     fn get_rev_index(idx: usize) -> usize {
         (7 - idx as isize).abs().try_into().unwrap()
@@ -187,11 +210,11 @@ impl Board {
 fn main() {
     let mut board = Board::new();
 
-    board.update_board(&Movement::parse_movement_notation("e2 e4"));
-    board.update_board(&Movement::parse_movement_notation("e7 e5"));
-    board.update_board(&Movement::parse_movement_notation("b1 c3"));
-    board.update_board(&Movement::parse_movement_notation("d7 d6"));
-    board.update_board(&Movement::parse_movement_notation("f1 c4"));
+    board.update_board(&Movement::parse_movement_notation("e2 e4").unwrap());
+    board.update_board(&Movement::parse_movement_notation("e7 e5").unwrap());
+    board.update_board(&Movement::parse_movement_notation("b1 c3").unwrap());
+    board.update_board(&Movement::parse_movement_notation("d7 d6").unwrap());
+    board.update_board(&Movement::parse_movement_notation("f1 c4").unwrap());
 
     board.render();
 }
